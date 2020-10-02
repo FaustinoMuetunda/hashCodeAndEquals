506368cf95943557863918dafd0985d70f351817
FaustinoPaulo <faustinomuetunda@gmail.com>
2020-10-02 12:17:53 +0100

    Adicionei o Objetos String. No primeiro instante, efetua a comparação dos inderecos do objeto. Já no segundo, compara o conteudo de cada objeto

--- src/hashcodeandequals/HashCodeandEquals.java
+++ src/hashcodeandequals/HashCodeandEquals.java
@@ -20,17 +20,45 @@ public class HashCodeandEquals {
         Cliente c1 = new Cliente("Faustino", "faustinomuetunda@gmail.com");
         Cliente c2 = new Cliente("Paulo", "faustinomuetunda@gmail.com");
         System.out.println("Equals: " + c1.equals(c2));
+        
+        
         int x = c1.hashCode();
         int y = c2.hashCode();
-        boolean z=false;
+        boolean z = false;
         
-        if(x==y){
+        
+        System.out.println("HashCode c1: " + x);
+        System.out.println("HashCode c2: " + y);
+
+        if (x == y) {
             System.out.println("HashCode true");
-        }else
+        } else {
             System.out.println("HashCode false");
-        System.out.println("HashCode c1: "+x);
-        System.out.println("HashCode c2: "+y);
-      
+        }
+        
+         System.out.println();
+
+        System.out.println("Testes");
+        /*
+        No primeiro caso, sera sempre falso, porque o compilador
+        estara a comparar endecos dos objestos e nao os objesto
+        */
+       
+
+        String s1 = new String("Test");
+        String s2 = new String("Test");
+        
+        /*
+        Neste instantes, teremos um resultado de acordo com o conteudo e não quanto
+        ao objeto em si.
+        */
+
+        String s3 = "Test";
+        String s4 = "Test";
+
+        System.out.println(s1 == s2);
+        System.out.println(s3==s4);
+
     }
 
 }
